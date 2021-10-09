6e725c0b1c9f8a5812235023d2208a40e17dff2c
Saurav Gandhi <49108365+sgandhi04@users.noreply.github.com>
2021-10-09 16:44:39 -0700

    Update and rename ObjectFollowing.py to OwnerTracking.py

--- OwnerTracking.py
+++ OwnerTracking.py
@@ -144,4 +144,68 @@ camera.unobserve_all()
 time.sleep(1.0)
 robot.stop()
 
+import torch
+import torchvision
+
+model = torchvision.models.alexnet(pretrained=False)
+model.classifier[6] = torch.nn.Linear(model.classifier[6].in_features, 2)
+
+model.load_state_dict(torch.load('best_model.pth'))
+device = torch.device('cuda')
+model = model.to(device)
+import cv2
+import numpy as np
+
+mean = 255.0 * np.array([0.485, 0.456, 0.406])
+stdev = 255.0 * np.array([0.229, 0.224, 0.225])
+
+normalize = torchvision.transforms.Normalize(mean, stdev)
+
+def preprocess(camera_value):
+    global device, normalize
+    x = camera_value
+    x = cv2.cvtColor(x, cv2.COLOR_BGR2RGB)
+    x = x.transpose((2, 0, 1))
+    x = torch.from_numpy(x).float()
+    x = normalize(x)
+    x = x.to(device)
+    x = x[None, ...]
+    return x
+import traitlets
+from IPython.display import display
+import ipywidgets.widgets as widgets
+from jetbot import Camera, bgr8_to_jpeg
+
+camera = Camera.instance(width=224, height=224)
+image = widgets.Image(format='jpeg', width=224, height=224)
+blocked_slider = widgets.FloatSlider(description='blocked', min=0.0, max=1.0, 
+
+import torch.nn.functional as F
+import time
+
+def update(change):
+    global blocked_slider, robot
+    x = change['new'] 
+    x = preprocess(x)
+    y = model(x)
+    
+    # we apply the `softmax` function to normalize the output vector so it sums to 1 (which makes it a probability distribution)
+    y = F.softmax(y, dim=1)
+    
+    prob_blocked = float(y.flatten()[0])
+    
+    blocked_slider.value = prob_blocked
+    
+    if prob_blocked < 0.5:
+        robot.forward(0.4)
+    else:
+        robot.left(0.4)
+    
+    time.sleep(0.001)
+        
+update({'new': camera.value})  # we call the function once to intialize
+                                     
+camera.observe(update, names='value')  # this attaches the 'update' function to the 'value' traitlet of our camera
+
+
 
